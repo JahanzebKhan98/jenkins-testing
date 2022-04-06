1cf178ac93e04100bd41d1e4033d5798dc5a315c
JahanzebKhan98 <80027665+JahanzebKhan98@users.noreply.github.com>
2022-04-06 15:48:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@
 # from webdriver_manager.chrome import ChromeDriverManager
 
 
-try:
-#   url="https://www.google.com/search?q=jennkins+PermissionError%3A+%5BErrno+13%5D+Permission+denied%3A+%27%2Fusr%2Flocal%2Fbin%2Fpip%27&oq=jennkins+PermissionError%3A+%5BErrno+13%5D+Permission+denied%3A+%27%2Fusr%2Flocal%2Fbin%2Fpip%27&aqs=chrome..69i57.3980j0j9&sourceid=chrome&ie=UTF-8"
-#   rep = requests.get(url)
-  print ('Hello World')
-except:
-  print ('error occured jk')
+# try:
+# #   url="https://www.google.com/search?q=jennkins+PermissionError%3A+%5BErrno+13%5D+Permission+denied%3A+%27%2Fusr%2Flocal%2Fbin%2Fpip%27&oq=jennkins+PermissionError%3A+%5BErrno+13%5D+Permission+denied%3A+%27%2Fusr%2Flocal%2Fbin%2Fpip%27&aqs=chrome..69i57.3980j0j9&sourceid=chrome&ie=UTF-8"
+# #   rep = requests.get(url)
+#   print ('Hello World')
+# except:
+print ('Hello World')
